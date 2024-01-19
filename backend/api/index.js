afbaa54e577ac250f4920c44602d7e48b564c37a
Eric Li <ericlitong@gmail.com>
2024-01-19 15:42:16 +0800

    update

--- backend/api/index.js
+++ backend/api/index.js
@@ -171,8 +171,8 @@ app.delete("/transactions/:transactionId", async (req, res) => {
 });
 
 // // Start the server
-// app.listen(port, () => {
-// 	console.log(`Server is running on http://localhost:${port}`);
-// });
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
 
 module.exports = app;
